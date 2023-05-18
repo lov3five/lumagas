be3d27fcb996a27e68b78f87dfe5f99d3be43031
lov3five <luongtranstaff@gmail.com>
2023-05-18 12:27:20 +0700

    Init skeleton source

---
+++ app.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+# Lấy đường dẫn gốc của dự án
+project_root = os.path.dirname(os.path.abspath(__file__))
+
+# Thêm đường dẫn gốc vào PYTHONPATH
+sys.path.append(project_root)
+
---
+++ db/service.py
@@ -0,0 +1,5 @@
+import mysql.connector
+
+class DatabaseService:
+    def __init__(self):
+        
