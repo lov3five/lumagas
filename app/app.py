35f13e6972377c1d64fbb68742dee9c16b0f90a6
lov3five <luongtranstaff@gmail.com>
2023-05-19 18:19:03 +0700

    API check file_type course, room, timelesson

--- app/app.py
+++ app/app.py
@@ -197,33 +197,33 @@ def upload_file(file_type):
     if len(request.files.getlist('file')) != 1:
         return jsonify({'result': 'Vui lòng chỉ tải lên một tệp'}), 400
     
-    # Kiểm tra phần mở rộng hợp lệ cho từng loại tệp
-    if not allowed_file(file.filename, file_type):
-        return jsonify({'result': f'File không hợp lệ, chỉ chấp nhận phần mở rộng {allowed_extensions[file_type]}'}, 400)
-    
-    # Đọc file mẫu dựa trên loại tệp
-    if file_type == 'course':
-        template_df = pd.read_excel('./app/static/file/templates/course_template.xlsx')
-    elif file_type == 'room':
-        template_df = pd.read_excel('./app/static/file/templates/room_template.xlsx')
-    elif file_type == 'timelesson':
-        template_df = pd.read_excel('./app/static/file/templates/timelesson_template.xlsx')
-    
-    # Kiểm tra file đầu vào và trả lỗi 
-    is_valid, error_data = check_file_data_input(df, template_df)
-    
-    if is_valid: 
-        # Lưu file vào thư mục dựa trên loại tệp
-        filename = secure_filename(file.filename)
-        unique_filename = get_unique_filename(filename)
-        save_path = get_save_path(file_type, unique_filename)
-        file.save(save_path)
-        return jsonify({'result': f'Tệp đã tải lên thành công với tên: {unique_filename}'}), 200
-    else: 
-        if error_data is not None:
-            return jsonify({'result': error_data}), 400
+    if request.method == 'POST' and file and allowed_file(file.filename):
+        # Đọc file mẫu dựa trên loại tệp
+        if file_type == 'course':
+            template_df = pd.read_excel('./app/static/file/templates/course_template.xlsx')
+        elif file_type == 'room':
+            template_df = pd.read_excel('./app/static/file/templates/room_template.xlsx')
+        elif file_type == 'timelesson':
+            template_df = pd.read_excel('./app/static/file/templates/timelesson_template.xlsx')
         else:
-            return jsonify({'result': 'Tệp không hợp lệ'}), 400
+            return jsonify({'result': 'Không hỗ trợ upload cho loại tệp này'}), 400
+        df = pd.read_excel(file)
+        # Kiểm tra file đầu vào và trả lỗi 
+        is_valid, error_data = check_file_data_input(df, template_df)
+        # is_valid = True | False
+        if is_valid: 
+            # Lưu file vào thư mục
+            filename = secure_filename(file.filename)
+            unique_filename = get_unique_filename(filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], unique_filename))
+            return jsonify({'result': 'Tệp đã tải lên thành công với tên: ' + unique_filename}), 200
+        else: 
+            if error_data is not None:
+                return jsonify({'result': error_data}), 400
+            else:
+                return jsonify({'result': 'Tệp không hợp lệ'}), 400
+    else: 
+        return jsonify({'result': 'File is invalid, only accept .xlsx, .xls, .csv'}), 400
             
 # # API upload file excel ROOM
 # @app.route('/api/upload/room', methods=['POST'])
